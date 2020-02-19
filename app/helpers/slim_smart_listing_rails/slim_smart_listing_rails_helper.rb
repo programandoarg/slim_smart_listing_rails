a89fac8bbdc9bc5906938b938aae7a1d16769826
lsantobuono <luciano.santobuono@gmail.com>
2020-02-19 15:08:59 -0300

    Hackeo de url y select en bip

--- app/helpers/slim_smart_listing_rails/slim_smart_listing_rails_helper.rb
+++ app/helpers/slim_smart_listing_rails/slim_smart_listing_rails_helper.rb
@@ -1,10 +1,11 @@
 module SlimSmartListingRails
   module SlimSmartListingRailsHelper
-    def editar_en_lugar(objeto, atributo, tipo = :input)
+    def editar_en_lugar(objeto, atributo, tipo = :input, _url = nil, collection = nil)
+      _url = editar_en_lugar_url(objeto) if _url.nil?
       if tipo == :checkbox
-        best_in_place objeto, atributo, url: editar_en_lugar_url(objeto), as: tipo, collection: ["No", "Si"], param: objeto.model_name.name
+        best_in_place objeto, atributo, url: _url, as: tipo, collection: ["No", "Si"], param: objeto.model_name.name
       elsif tipo == :date
-        best_in_place objeto, atributo, url: editar_en_lugar_url(objeto), as: tipo, display_with: lambda { |v| dmy(v) }, param: objeto.model_name.name
+        best_in_place objeto, atributo, url: _url, as: tipo, display_with: lambda { |v| dmy(v) }, param: objeto.model_name.name
         # best_in_place objeto, atributo, url: editar_en_lugar_url(objeto), as: tipo, display_with: lambda { |v| dmy(v) }, class: 'datefield'
       elsif tipo == :textarea
         funcion = lambda do |valor|
@@ -13,9 +14,11 @@ module SlimSmartListingRails
           valor.gsub!("\n", '<br>')
           valor.html_safe
         end
-        best_in_place objeto, atributo, url: editar_en_lugar_url(objeto), as: tipo, display_with: funcion, param: objeto.model_name.name
+        best_in_place objeto, atributo, url: _url, as: tipo, display_with: funcion, param: objeto.model_name.name
+      elsif tipo == :select && collection.present?
+        best_in_place objeto, atributo, url: _url, as: tipo, collection: collection, param: objeto.model_name.name, value: objeto.send(atributo)
       else
-        best_in_place objeto, atributo, url: editar_en_lugar_url(objeto), as: tipo, param: objeto.model_name.name
+        best_in_place objeto, atributo, url: _url, as: tipo, param: objeto.model_name.name
       end
     end
   end
